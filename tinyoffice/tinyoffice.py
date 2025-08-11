852da4ccca84088aad62a55473356da9bc5d584f
Samuel J. Woodward <sam@woodward.fyi>
2025-08-11 10:51:10 -0400

    Improved messaging

--- tinyoffice/tinyoffice.py
+++ tinyoffice/tinyoffice.py
@@ -36,6 +36,11 @@ class Verbosity(enum.Enum):
     HIGH = enum.auto()
 
 
+GB = 1024 * 1024 * 1024
+MB = 1024 * 1024
+KB = 1024
+
+
 def walk(
     cwd,
     *,
@@ -464,45 +469,25 @@ def printer(future, verbosity=Verbosity.NORMAL):
     except Exception as e:
         print(e)
     else:
-        plural_errors = '' if len(result.errors) == 1 else 's'
         if verbosity is Verbosity.LOW:
             if result.num_images_compressed:
-                print(
-                    f'Compressed {result.filename!r}. '
-                    f'{len(result.errors):,} Error{plural_errors} encountered.'
-                )
+                print(f'Compressed {result.filename!r}')
         elif verbosity is Verbosity.NORMAL:
-            if result.num_images_compressed:
-                print(
-                    f'Filename: {result.filename!r}.'
-                    '\n'
-                    f'Results: '
-                    f'{result.num_images_compressed:,} compressed, '
-                    f'{result.num_images_converted:,} converted, '
-                    f'{result.num_images_skipped:,} skipped'
-                    '\n'
-                    f'Errors: {len(result.errors):,}'
-                )
-            else:
-                print(
-                    f'No images compressed for {result.filename!r}. '
-                    f'{len(result.errors):,} Error{plural_errors} encountered.'
-                )
+            print(
+                f'Filename: {result.filename!r}.\n\tResults: '
+                f'{result.num_images_compressed:,} compressed, '
+                f'{result.num_images_converted:,} converted, '
+                f'{result.num_images_skipped:,} skipped, '
+                f'{len(result.errors):,} errors'
+            )
         elif verbosity is Verbosity.HIGH:
             errors = ", ".join(result.errors) if result.errors else 'None!'
             print(
-                ''.join(
-                    [
-                        f'Filename: {result.filename!r}.',
-                        '\n',
-                        'Results: ',
-                        f'\n\t{result.num_images_compressed:,} compressed',
-                        f'\n\t{result.num_images_converted:,} converted',
-                        f'\n\t{result.num_images_skipped:,} skipped',
-                        '\n',
-                        f'Errors:\n\t{errors}'
-                    ]
-                )
+                f'Filename: {result.filename!r}.\n\tResults: '
+                f'\n\t\t{result.num_images_compressed:,} compressed'
+                f'\n\t\t{result.num_images_converted:,} converted'
+                f'\n\t\t{result.num_images_skipped:,} skipped'
+                f'\n\tErrors:\n\t\t{errors}'
             )
 
 
@@ -530,94 +515,99 @@ def totaler(output_record, future):
 
 
 def print_total(record, verbosity):
-    print('\n\n')
     plural_files = '' if len(record['compressed_files']) == 1 else 's'
     plural_imgs = '' if record['images_compressed'] == 1 else 's'
     plural_converted = '' if record['images_converted'] == 1 else 's'
     plural_img_errs = '' if record['image_errors'] == 1 else 's'
     plural_errs = '' if len(record['errors']) == 1 else 's'
+    output = ['\n']
     if verbosity is Verbosity.LOW:
-        print(
+        output.append(
             f'Compressed {len(record["compressed_files"]):,} '
-            f'document{plural_files} with '
-            f'{len(record["image_errors"]):,} '
+            f'document{plural_files} with {len(record["image_errors"]):,} '
             f'image{plural_img_errs} that could not be converted and '
-            f'{len(record["errors"]):,} document{plural_errs} '
-            'that failed.'
+            f'{len(record["errors"]):,} document{plural_errs} that failed.'
         )
     elif verbosity is Verbosity.NORMAL:
         if record['total_bytes_compressed'] > 0:
-            gb = 1024 * 1024 * 1024
-            mb = 1024 * 1024
-            kb = 1024
             total_cmp = record['total_bytes_compressed']
-            if total_cmp > gb:
-                savings = f'{total_cmp / gb:.2f} GB'
-            elif total_cmp > mb:
-                savings = f'{total_cmp / mb:.2f} MB'
-            elif total_cmp > kb:
-                savings = f'{total_cmp / kb:.2f} KB'
+            if total_cmp > GB:
+                savings = f'{total_cmp / GB:.2f} GB'
+            elif total_cmp > MB:
+                savings = f'{total_cmp / MB:.2f} MB'
+            elif total_cmp > KB:
+                savings = f'{total_cmp / KB:.2f} KB'
             else:
                 if total_cmp < 1:
                     total_cmp = '<1'
                 savings = f'{total_cmp} bytes'
-            output = f'Compressed {len(record["compressed_files"]):,} '
-            output += f'document{plural_files}\n'
-            output += f'{record["images_compressed"]:,} '
-            output += f'image{plural_imgs} were '
-            output += f'compressed for a savings of {savings}'
-            output += '\n'
+            output.append(
+                f'Compressed {len(record["compressed_files"]):,} '
+                f'document{plural_files} with {record["images_compressed"]:,} '
+                f'image{plural_imgs} being '
+                f'compressed for a savings of {savings}'
+            )
+            # Maybe add these regardless
             if record['images_converted'] > 0:
-                output += f'{record["images_converted"]:,} '
-                output += f'image{plural_converted} were '
-                output += 'converted from TIFF to JPG\n'
+                output.append(
+                    f'{record["images_converted"]:,} '
+                    f'image{plural_converted} were '
+                    'converted from TIFF to JPG'
+                )
             if record['image_errors']:
-                output += f'{len(record["image_errors"]):,} '
-                output += f'image{plural_img_errs} could not be '
-                output += 'converted or compressed\n'
+                output.append(
+                    f'{len(record["image_errors"]):,} '
+                    f'image{plural_img_errs} could not be '
+                    'converted or compressed'
+                )
             if record['errors']:
-                output += f'{len(record["errors"]):,} '
-                output += f'document{plural_errs} '
-                output += 'could not be compressed due to error'
+                output.append(
+                    f'{len(record["errors"]):,} '
+                    f'document{plural_errs} '
+                    'could not be compressed due to error'
+                )
         else:
-            output = 'No images were compressed'
-        print(output)
+            output.append('No images were compressed')
     elif verbosity is Verbosity.HIGH:
         if record['total_bytes_compressed'] > 0:
-            gb = 1024 * 1024 * 1024
-            mb = 1024 * 1024
-            kb = 1024
             total_cmp = record['total_bytes_compressed']
-            if total_cmp > gb:
-                savings = f'{total_cmp / gb:.2f} GB'
-            elif total_cmp > mb:
-                savings = f'{total_cmp / mb:.2f} MB'
-            elif total_cmp > kb:
-                savings = f'{total_cmp / kb:.2f} KB'
+            if total_cmp > GB:
+                savings = f'{total_cmp / GB:.2f} GB'
+            elif total_cmp > MB:
+                savings = f'{total_cmp / MB:.2f} MB'
+            elif total_cmp > KB:
+                savings = f'{total_cmp / KB:.2f} KB'
             else:
                 if total_cmp < 1:
                     total_cmp = '<1'
                 savings = f'{total_cmp} bytes'
-            output = f'Compressed {len(record["compressed_files"]):,} '
-            output += f'document{plural_files}:\n\t'
-            output += '\n\t'.join(record['compressed_files'])
-            output += f'\n{record["images_compressed"]:,} '
-            output += f'image{plural_imgs} were '
-            output += f'compressed for a savings of {savings}'
-            output += '\n'
+            nl_t = '\n\t'
+            output.append(
+                f'Compressed {len(record["compressed_files"]):,} '
+                f'document{plural_files}:\n\t'
+                f'{nl_t.join(repr(i) for i in record["compressed_files"])}'
+                f'\n{record["images_compressed"]:,} '
+                f'image{plural_imgs} were '
+                f'compressed for a savings of {savings}'
+            )
             if record['images_converted'] > 0:
-                output += f'{record["images_converted"]:,} '
-                output += f'image{plural_converted} were converted '
-                output += 'from TIFF to JPG\n'
+                output.append(
+                    f'{record["images_converted"]:,} '
+                    f'image{plural_converted} were converted '
+                    'from TIFF to JPG'
+                )
             if record['image_errors']:
-                output += f'{len(record["image_errors"]):,} '
-                output += f'image{plural_img_errs} could not be '
-                output += 'converted or compressed\n'
+                output.append(
+                    f'{len(record["image_errors"]):,} '
+                    f'image{plural_img_errs} could not be '
+                    'converted or compressed'
+                )
             if record['errors']:
-                output += 'ERRORS:\n\t'
-                output += '\n\t'.join(record["errors"])
+                output.append(
+                    f'ERRORS:\n\t{nl_t.join(record["errors"])}'
+                )
             else:
-                output += 'No errors received!'
+                output.append('No errors received!')
         else:
-            output = 'No images were compressed'
-        print(output)
+            output.append('No images were compressed')
+    print('\n'.join(output))
